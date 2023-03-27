ad64cc21d34b95852508b0aecad6500554da3e8e
Artur Peiev <fromtheisland13@gmail.com>
2023-03-27 22:26:16 +0100

    Npm_task

---
+++ js/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const num = 1;
+const num1 = 2;
+
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const minus = (a, b) => a - b;
+
+console.log(minus(num, num1));
+console.log(sum(num, num1));
+console.log(multiply(num, num1));
